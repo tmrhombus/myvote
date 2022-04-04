82f5b4df54360d5d1a8d3dbc9f0ea51b483a1097
Tom Perry <tomperry7@gmail.com>
2022-04-03 21:07:50 -0600

    pops2020 file

---
+++ pops_2020.py
@@ -0,0 +1,109 @@
+
+pops = [
+# 2016 july estimated 
+4863300 , #   Alabama 	
+741894 	, #   Alaska    	
+6931071 , #   Arizona 	
+2988248 , #   Arkansas 	
+39250017, #   California 	
+5540545 , #   Colorado 	
+3576452 , #   Connecticut 	
+952065  , #   Delaware 	
+681170  , #   DC   
+20612439, #   Florida 	
+10310371, #   Georgia 	
+1428557 , #   Hawaii   	
+1683140 , #   Idaho   	
+12801539, #   Illinois 	
+6633053 , #   Indiana 	
+3134693 , #   Iowa    	
+2907289 , #   Kansas   	
+4436974 , #   Kentucky 	
+4681666 , #   Louisiana 	
+1331479 , #   Maine   	
+6016447 , #   Maryland 	
+6811779 , #   Massachusetts 	
+9928301 , #   Michigan 	
+5519952 , #   Minnesota 	
+2988726 , #   Mississippi 	
+6093000 , #   Missouri 	
+1042520 , #   Montana 	
+1907116 , #   Nebraska 	
+2940058 , #   Nevada   	
+1334795 , #   New Hampshire 	
+8944469 , #   New Jersey 	
+2081015 , #   New Mexico 	
+19745289, #   New York 	
+10146788, #   North Carolina 	
+757952  , #   North Dakota 	
+4961119 , #   South Carolina 	
+865454  , #   South Dakota 	
+11614373, #   Ohio    	
+3923561 , #   Oklahoma 	
+4093465 , #   Oregon   	
+12802503, #   Pennsylvania 	
+1056426 , #   Rhode Island 	
+6651194 , #   Tennessee 	
+27862596, #   Texas   	
+3051217 , #   Utah     	
+624594  , #   Vermont    	
+8411808 , #   Virginia 	
+7288000 , #   Washington 	
+1831102 , #   West Virginia 	
+5778708 , #   Wisconsin 	
+585501 	, #   Wyoming 	
+]
+
+
+# # 2010 census
+# 4780127   Alabama 	
+# 710249    Alaska    	
+# 6392307   Arizona 	
+# 2915958   Arkansas 	
+# 37254503  California 	
+# 5029324   Colorado 	
+# 3574118   Connecticut 	
+# 897936    Delaware 	
+# 601767    DC
+# 18804623  Florida 	
+# 9688681   Georgia 	
+# 1360301   Hawaii   	
+# 1567652   Idaho   	
+# 12831549  Illinois 	
+# 6484229   Indiana 	
+# 3046869   Iowa    	
+# 2853132   Kansas   	
+# 4339349   Kentucky 	
+# 4533479   Louisiana 	
+# 1328361   Maine   	
+# 5773785   Maryland 	
+# 6547817   Massachusetts 	
+# 9884129   Michigan 	
+# 5303925   Minnesota 	
+# 2968103   Mississippi 	
+# 5988927   Missouri 	
+# 989417    Montana 	
+# 1826341   Nebraska 	
+# 2700691   Nevada   	
+# 1316466   New Hampshire 	
+# 8791936   New Jersey 	
+# 2059192   New Mexico 	
+# 19378087  New York 	
+# 9535692   North Carolina 	
+# 672591    North Dakota 	
+# 4625401   South Carolina 	
+# 814191    South Dakota 	
+# 11536725  Ohio    	
+# 3751616   Oklahoma 	
+# 3831073   Oregon   	
+# 12702887  Pennsylvania 	
+# 1052931   Rhode Island 	
+# 6346275   Tennessee 	
+# 25146105  Texas   	
+# 2763888   Utah     	
+# 625745    Vermont    	
+# 8001045   Virginia 	
+# 6724543   Washington 	
+# 1853011   West Virginia 	
+# 5687289   Wisconsin 	
+# 563767    Wyoming 	
